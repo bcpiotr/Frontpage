ae4ae45e43ee7e5d20db9d5fc9d501e1eab21f5e
bcpiotr <piotr.nowak@post.com>
2016-03-03 16:46:55 +0100

    uzupelnione eventy buttonow i ostylowane buttony

--- js/script.js
+++ js/script.js
@@ -17,6 +17,11 @@ function mozaicAnimation(color){
     $(this).fadeIn( delayTime, function() {
     // Animation complete
   });
+  //ustawia kolory ostatnim panelom
+  panel.last().css('backgroundColor', '#737778');
+  panel.eq(-2).css('backgroundColor', '#737778');
+  panel.eq(-3).css('backgroundColor', '#737778');
+  panel.eq(-4).css('backgroundColor', '#737778');
   });
 }
 
@@ -41,9 +46,30 @@ PANEL RESIZE FUNCTION
 USTAWIA BUTTONY NA 4 OSTATNICH PANELACH
 ****************************************/
 panel.last().attr('id', 'btnContact');
+panel.last().css('justifyContent', 'center');
+panel.last().css('alignItems', 'center');
+panel.last().css('backgroundColor', '#737778');
+panel.last().html('<i class="fa fa-envelope fa-2x"></i>');
+
 panel.eq(-2).attr('id', 'btnPortfolio');
+panel.eq(-2).css('justifyContent', 'center');
+panel.eq(-2).css('alignItems', 'center');
+panel.eq(-2).css('backgroundColor', '#737778');
+panel.eq(-2).html('<i class="fa fa-folder fa-2x"></i>');
+
 panel.eq(-3).attr('id', 'btnResume');
+panel.eq(-3).css('justifyContent', 'center');
+panel.eq(-3).css('alignItems', 'center');
+panel.eq(-3).css('backgroundColor', '#737778');
+panel.eq(-3).html('<i class="fa fa-graduation-cap fa-2x"></i>');
+
+
 panel.eq(-4).attr('id', 'btnAbout');
+panel.eq(-4).css('justifyContent', 'center');
+panel.eq(-4).css('alignItems', 'center');
+panel.eq(-4).css('backgroundColor', '#E9F0F2');
+panel.eq(-4).html('<i class="fa fa-user fa-2x"></i>');
+
 
 
 /****************************************
@@ -55,27 +81,34 @@ $('#btnAbout').on('click',function(){
   fadeOutContent();
   fadeInContent(about_main);
   fadeInContent(about_side);
-  console.log('test');
+  $(this).css('backgroundColor','#E9F0F2');
 });
 
 ///RESUME
 $('#btnResume').on('click',function(){
-  mozaicAnimation("yellow");
+  mozaicAnimation("white");
   fadeOutContent();
   fadeInContent(resume);
-    console.log('test');
+  $(this).css('backgroundColor','#E9F0F2');
+
 });
 
 //PORTFOLIO
 $('#btnPortfolio').on('click',function(){
-  mozaicAnimation("red");
-  console.log('test');
+  mozaicAnimation("white");
+  fadeOutContent();
+  fadeInContent(portfolio);
+  $(this).css('backgroundColor','#E9F0F2');
+
 });
 
 //contact
 $('#btnContact').on('click',function(){
-  mozaicAnimation("blue");
-  console.log('test');
+  mozaicAnimation("white");
+  fadeOutContent();
+  fadeInContent(contact);
+  $(this).css('backgroundColor','#E9F0F2');
+
 })
 
 /*************************************
