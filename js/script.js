c1cd05e70d4237d7ab2ac91f948dce15a8652e97
bcpiotr <piotr.nowak@post.com>
2016-03-04 13:34:40 +0100

    optymalizacja i dodatkowe opisy kodu cz.1

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 $(function(){
   var panel = $('.panel');
   var main = $('.main');
-  console.log(panel);
 
-mozaicAnimation("white");
+
+/***************************
+WYWOLYWANIE FUNKCJI PO OTWARCIU STRONY
+**************************/
+mozaicAnimation("whiteBckg");
+setButtons();
+
 
 /**********************
-MOZAIC Animation
+FUNCTION - MOZAIC Animation
 **********************/
-
 function mozaicAnimation(color){
   panel.each(function( index ) {
     var delayTime = Math.floor((Math.random() * 5000) + 1000);
     $(this).css('display', 'none');
-    $(this).css('backgroundColor', color);
-    $(this).fadeIn( delayTime, function() {
+    // $(this).addClass('whiteBckg');
+    $(this).delay(1000).fadeIn( delayTime, function() {
     // Animation complete
+
   });
-  //ustawia kolory ostatnim panelom
-  panel.last().css('backgroundColor', '#737778');
-  panel.eq(-2).css('backgroundColor', '#737778');
-  panel.eq(-3).css('backgroundColor', '#737778');
-  panel.eq(-4).css('backgroundColor', '#737778');
   });
 }
 
 
 /*************************
-PANEL RESIZE FUNCTION
+FUNCTION PANEL RESIZE
 *************************/
 //przy wejsciu sprawdza szerokosc 'main' i ew. zmienia css height dla panelu
   var width = main.width()/10;
@@ -35,80 +35,67 @@ PANEL RESIZE FUNCTION
     panel.css("height", width);
   }
 
-//Przy resize zmienia w css height panelu
+//Przy resize zmienia w css height i width panelu
   $(window).on('resize',function(){
     width = main.width()/10;
     console.log(width);
-    panel.css("height", width);
+    panel.css("height", width, "width", width);
   })
 
 /*****************************************
-USTAWIA BUTTONY NA 4 OSTATNICH PANELACH
+FUNCTION SET BUTTONS - ustawia buttony na ostatnich panelach
 ****************************************/
-panel.last().attr('id', 'btnContact');
-panel.last().css('justifyContent', 'center');
-panel.last().css('alignItems', 'center');
-panel.last().css('backgroundColor', '#737778');
-panel.last().html('<i class="fa fa-envelope fa-2x"></i>');
-
-panel.eq(-2).attr('id', 'btnPortfolio');
-panel.eq(-2).css('justifyContent', 'center');
-panel.eq(-2).css('alignItems', 'center');
-panel.eq(-2).css('backgroundColor', '#737778');
-panel.eq(-2).html('<i class="fa fa-folder fa-2x"></i>');
-
-panel.eq(-3).attr('id', 'btnResume');
-panel.eq(-3).css('justifyContent', 'center');
-panel.eq(-3).css('alignItems', 'center');
-panel.eq(-3).css('backgroundColor', '#737778');
-panel.eq(-3).html('<i class="fa fa-graduation-cap fa-2x"></i>');
+function setButtons(){
+  panel.last().attr('id', 'btnContact');
+  panel.last().html('<i class="fa fa-envelope fa-2x"></i>');
 
 
-panel.eq(-4).attr('id', 'btnAbout');
-panel.eq(-4).css('justifyContent', 'center');
-panel.eq(-4).css('alignItems', 'center');
-panel.eq(-4).css('backgroundColor', '#E9F0F2');
-panel.eq(-4).html('<i class="fa fa-user fa-2x"></i>');
+  panel.eq(-2).attr('id', 'btnPortfolio');
+  panel.eq(-2).html('<i class="fa fa-folder fa-2x"></i>');
 
+  panel.eq(-3).attr('id', 'btnResume');
+  panel.eq(-3).html('<i class="fa fa-graduation-cap fa-2x"></i>');
+
+  panel.eq(-4).attr('id', 'btnAbout');
+  panel.eq(-4).css('backgroundColor', '#E9F0F2'); //przy pierwszym wywoalniu ustawia jasny kolor dla about
+  panel.eq(-4).html('<i class="fa fa-user fa-2x"></i>');
+}
 
 
 /****************************************
-AKCJE BUTTONOW
+AKCJE BUTTONOW - ONCLICK
 ***************************************/
-//ABOUT
-$('#btnAbout').on('click',function(){
+$('div[id^="btn"]').on('click',function(){
   mozaicAnimation("white");
   fadeOutContent();
-  fadeInContent(about_main);
-  fadeInContent(about_side);
-  $(this).css('backgroundColor','#E9F0F2');
-});
+  $('div[id^="btn"]').css('backgroundColor', '#737778');
+  $(this).css('backgroundColor', '#E9F0F2');
 
-///RESUME
-$('#btnResume').on('click',function(){
-  mozaicAnimation("white");
-  fadeOutContent();
-  fadeInContent(resume);
-  $(this).css('backgroundColor','#E9F0F2');
+  var btnId = $(this).attr('id');
+  //sprawdza po ID jaki button zostal nacisniety i laduje odpowiedni content
+  if (btnId === "btnAbout") {
+      fadeInContent(about_main);
+      fadeInContent(about_side);
+  }
+  else if (btnId === 'btnResume') {
+      fadeInContent(resume);
+  }
+  else if (btnId === 'btnPortfolio') {
+      fadeInContent(portfolio);
+  }
+  else if (btnId === 'btnContact') {
+      fadeInContent(contact);
+  }
+})
 
-});
-
-//PORTFOLIO
-$('#btnPortfolio').on('click',function(){
-  mozaicAnimation("white");
-  fadeOutContent();
-  fadeInContent(portfolio);
-  $(this).css('backgroundColor','#E9F0F2');
-
-});
-
-//contact
-$('#btnContact').on('click',function(){
-  mozaicAnimation("white");
-  fadeOutContent();
-  fadeInContent(contact);
-  $(this).css('backgroundColor','#E9F0F2');
-
+/***********************************
+AKCJE BUTTONOW - MOUSEOVER/MOUSEOUT
+***********************************/
+$('div[id^="btn"]').on('mouseover', function(){
+  $(this).css('backgroundColor', '#E9F0F2')
+})
+$('div[id^="btn"]').on('mouseout', function(){
+  $(this).css('backgroundColor', '#737778')
 })
 
 /*************************************
@@ -116,7 +103,6 @@ ALL FADE OUT FUNCTION
 ************************************/
 function fadeOutContent (){
   $('.main').find('.content').fadeOut(3000,function(){
-    console.log('huraaaa');
   });
 }
 
@@ -125,9 +111,10 @@ FADE IN FUNCTION
 ***************************************/
 function fadeInContent (id){
   $(id).fadeIn(3000, function(){
-    console.log("jazda");
   })
 }
+
+
 
 
 });
